5fc119cfcfffeb2189c10609cbc0bca99feaef98
MIKNSJ <sculetoncode@gmail.com>
2024-05-16 18:35:59 -0700

    files init

---
+++ src/game.cpp
@@ -0,0 +1,23 @@
+/**
+ *
+ * DATE: 2024-05-16 - 2024-05-XX
+ * PURPOSE: Generates the minesweeper game.
+ *
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+/**
+ * Main function
+ * @return 0 = success, 1 = failure
+ */
+int main() {
+    cout << "This is a test message!" << endl;
+
+    return 0;
+}
