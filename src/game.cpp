3cf0f662b525a4d00c3260bd637103067df1b62c
MIKNSJ <sculetoncode@gmail.com>
2024-05-16 23:59:39 -0700

    generate_mines() init and completed for three difficulties

--- src/game.cpp
+++ src/game.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 
@@ -22,15 +24,19 @@ using namespace std;
 class Game {
     public:
         unsigned int score;
+        unsigned int difficulty;
         string username;
         vector<vector<char>> board;
 
         /**
          * PURPOSE: Generates the game.
          */
-        Game(unsigned int size) : score(0), username("null"), board(size) {
+        Game(unsigned int size) : score(0), difficulty(0),
+            username("null"), board(size) {
 
         }
+
+
 
         /**
          * PURPOSE: Generates the playing board.
@@ -48,19 +54,68 @@ class Game {
             }
             cout << "==============================" << endl;
         }
+        
+
+
+        /**
+         * PURPOSE: Generates mines on the board.
+         * @param board The playing board.
+         * @return Nothing
+         */
+        void generate_mines(vector<vector<char>> &board,
+                unsigned int &difficulty) {
+            unsigned int mines_count = 0;
+            unsigned int row_num;
+            unsigned int col_num;
+            unsigned int row_sel;
+            unsigned int col_sel;
+
+            switch(difficulty) {
+                case 1:
+                    mines_count = 10;
+                    row_num = 16;
+                    col_num = 16;
+                    break;
+                case 2:
+                    mines_count = 99;
+                    row_num = 16;
+                    col_num = 30;
+                    break;
+                default:
+                    mines_count = 10;
+                    row_num = 9;
+                    col_num = 9;
+                    break;
+            }
+
+            for (int i = 0; i < (int)mines_count; i++) {
+                row_sel = rand() % (row_num - 1);
+                col_sel = rand() % (col_num - 1);
+                board[row_sel][col_sel] = '*';
+                
+                cout << "A mine has been set at (" << row_sel << ","
+                    << col_sel << ")." << endl;
+            }
+        }
 };
 
 
 
 /**
- * Main function
+ * PURPOSE: Main function
  * @return 0 = success, 1 = failure
  */
 int main() {
     vector<vector<char>> test = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
     Game test_game(1);
-    test_game.board = test;
-    test_game.draw_board(test);
+//  test_game.board = test;
+//  test_game.draw_board(test);
+
+    vector<vector<char>> test_two(9, vector<char> (9, '1'));
+    test_game.board = test_two;
+    test_game.draw_board(test_game.board);
+    test_game.generate_mines(test_game.board, test_game.difficulty);
+    test_game.draw_board(test_game.board);
     cout << "Success" << endl;
 
     return 0;
